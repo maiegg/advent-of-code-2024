265d96ddbaef5723cef4cb95acad653932ad536d
maiegg <maggieowilson@gmail.com>
2025-01-20 11:39:34 -0800

    day4 part2

--- days/day4.py
+++ days/day4.py
@@ -54,4 +54,54 @@ def solve():
 
     print(f'Part 1: {word_found_count}')
 
+    # Part 2: "X-MAS"
+    # X-MAS can appear with the M's starting in any 4 corners and continuing diagonally 
+    # We require that both diagonals be filled with "MAS"
+    
+    # First approach: search for "MAS" just like above and try to link two diagonal instances of "MAS" (futzy)
+    # Second approach: more efficient, search for A and then search outwards 
 
+    word_found_count = 0 
+
+    for i in range(grid_rows):
+        for j in range(grid_cols):
+            if grid[i][j] == 'A':
+
+                # Two possible configurations for each diagonal
+                # S in top right and M in bottom left OR vice versa (TR-BL)
+                # S in top left and M in bottom right OR vice versa (TL-BR)
+
+                bl_i, bl_j = i-1, j+1
+                tl_i, tl_j = i-1, j-1
+                tr_i, tr_j = i+1, j-1
+                br_i, br_j = i+1, j+1
+
+                # first check out of bounds; if so, pass
+                if (
+                    bl_i < 0 or bl_i >= grid_rows or 
+                    bl_j < 0 or bl_j >= grid_cols or
+                    tl_i < 0 or tl_i >= grid_rows or 
+                    tl_j < 0 or tl_j >= grid_cols or
+                    tr_i < 0 or tr_i >= grid_rows or 
+                    tr_j < 0 or tr_j >= grid_cols or
+                    br_i < 0 or br_i >= grid_rows or 
+                    br_j < 0 or br_j >= grid_cols   
+                ):
+                    continue 
+                elif (
+                    (
+                        # condition 1: the TR-BL diagonal is "MAS" in either direction 
+                        (grid[bl_i][bl_j] == 'M' and grid [tr_i][tr_j] == 'S') 
+                        or 
+                        (grid[bl_i][bl_j] == 'S' and grid [tr_i][tr_j] == 'M')
+                    )
+                    and (
+                        # condition 2: the TL-BR diagonal is "MAS" in either direction 
+                        (grid[br_i][br_j] == 'M' and grid [tl_i][tl_j] == 'S') 
+                        or 
+                        (grid[br_i][br_j] == 'S' and grid [tl_i][tl_j] == 'M')                 
+                    )
+                ):
+                    word_found_count += 1 
+
+    print(f'Part 2: {word_found_count}')
